3fcdc29ad474ab4df3502e13b1dc51a4a7e04a73
Blurry <blurry2746@blurry2746.com>
2025-08-06 22:21:46 -0700

    trying to make pixoo_handler a singleton

--- gui.py
+++ gui.py
@@ -3,66 +3,53 @@ from PyQt5.QtWidgets import (
     QMainWindow,
     QSystemTrayIcon,
     QMenu,
-    QStyle,
-    QMessageBox
+    QStyle
 )
 from PyQt5.QtCore import QTimer
-
+from pixoo_handler import PixooHandler  # Singleton PixooHandler
 
 class DivoomDNDGUI(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        self.setWindowTitle("DivoomDND Status Manager")
         self.setMinimumSize(400, 300)
         self.resize(600, 400)
-        self.setWindowTitle("DivoomDND Status Manager")
-        # Setup system tray icon using a default system icon
+
+        # Setup system tray icon
         default_icon = QApplication.style().standardIcon(QStyle.SP_ComputerIcon)
         self.tray_icon = QSystemTrayIcon(default_icon, self)
         self.tray_icon.setToolTip("DivoomDND")
 
-        # Create tray menu
         self.tray_menu = QMenu(self)
-        self.tray_icon.activated.connect(self.on_tray_icon_activated)
-        exit_action = self.tray_menu.addAction("Exit")
-        exit_action.triggered.connect(QApplication.quit)
         show_action = self.tray_menu.addAction("Show")
         show_action.triggered.connect(self.show)
+        exit_action = self.tray_menu.addAction("Exit")
+        exit_action.triggered.connect(QApplication.quit)
         self.tray_icon.setContextMenu(self.tray_menu)
+        self.tray_icon.activated.connect(self.on_tray_icon_activated)
         self.tray_icon.show()
 
-        # Start the periodic update timer
+        # Use singleton PixooHandler
+        self.pixoo_handler = PixooHandler()
+
+        # Start periodic updates
         self.start_timer()
 
     def update_status_display(self):
-        """
-        This method should contain the logic to update the Pixoo display
-        based on current status, priority, etc.
-        """
-        # TODO: Add your display update logic here
+        self.pixoo_handler.display_status("Status update message")
 
     def start_timer(self):
-        """
-        Starts a QTimer that calls update_status_display every 5 seconds.
-        """
         self.timer = QTimer(self)
-        self.timer.setInterval(5000)  # 5 seconds
+        self.timer.setInterval(5000)
         self.timer.timeout.connect(self.update_status_display)
         self.timer.start()
 
     def closeEvent(self, event):
-        """
-        Override the window close event to hide the window instead of exiting the app.
-        """
         event.ignore()
         self.hide()
 
     def on_tray_icon_activated(self, reason):
-        """
-        Handle tray icon activation (clicks).
-        """
-        if reason == QSystemTrayIcon.Trigger:  # Single click
-            self.showNormal()
-            self.activateWindow()
-        elif reason == QSystemTrayIcon.DoubleClick:  # Optional: handle double click separately
+        if reason == QSystemTrayIcon.Trigger:
             self.showNormal()
             self.activateWindow()
--- pixoo_handler.py
+++ pixoo_handler.py
@@ -1,11 +1,42 @@
 #	Handles communication with Pixoo64
 from vendor.pixoo_ng import Pixoo
 
-def update_pixoo_display(status, gif_path=None):
-    device = Pixoo()
 
-    device.clear()
-    device.draw_text(status)
+class PixooHandler:
+    _instance = None
 
-    if gif_path:
-        device.draw_image(gif_path)
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(PixooHandler, cls).__new__(cls)
+        return cls._instance
+
+    def __init__(self, *args, **kwargs):
+        if getattr(self, '_initialized', False):
+            return  # Prevent reinitialization
+
+        # Instantiate Pixoo only once
+        self.pixoo = Pixoo(*args, **kwargs)
+
+        # Optional: initialize other state variables
+        self._initialized = True
+
+    def display_status(self, status_text):
+        """
+        Display a status message on the Pixoo device.
+        """
+        self.pixoo.draw_text(status_text)
+        self.pixoo.push()
+
+    def display_gif(self, gif_path):
+        """
+        Display a GIF on the Pixoo device.
+        """
+        self.pixoo.draw_gif(gif_path)
+        self.pixoo.push()
+
+    def clear_display(self):
+        """
+        Clear the Pixoo display.
+        """
+        self.pixoo.clear()
+        self.pixoo.push()
